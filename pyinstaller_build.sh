56283d42f6e7e50a291f57e54101b76066d896e8
Wilbur Jaywright <zargulthewizard@outlook.com>
2025-09-04 15:44:37 -0400

    Improving exe build system

--- pyinstaller_build.sh
+++ pyinstaller_build.sh
@@ -1,5 +1,19 @@
+echo "Preparing venv"
+python -m venv .venv
+
+echo "Activating venv"
+source .venv/bin/activate
+
+echo "Installing requirements"
+pip install -r requirements.txt
+pip install -U setuptools pyinstaller
+
 echo "Building exe"
-pyinstaller -F --add-data pre-trimmed:pre-trimmed solar_rift_music_player.py
+pyinstaller -F --add-data pre-trimmed:pre-trimmed solar_rift_music_player.py 2>&1 | tee pyinstaller_build_log.txt
+
 echo "Cleaning up exe build residue"
 rm -rf build
 rm *.spec
+
+echo "Deactivating venv"
+deactivate
